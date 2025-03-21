e5134a0b9bc93405f6457d77df3aeae50b3d2487
lemi9090 <142860891+lemi9090@users.noreply.github.com>
2025-03-21 18:13:42 +0900

    Update sniff.c

--- sniff_c/sniff.c
+++ sniff_c/sniff.c
@@ -10,15 +10,16 @@
 // 2. 패킷 캡처 시작
 // 3. pcap 핸들 닫기
 
-// typedef unsigned char u_char; // 1바이트 메모리를 사용할 때 양수만 사용하여 값을 저장   
-// typedef unsigned short u_short; // 2바이트 메모리를 사용할 때 양수만 사용하여 값을 저장 
+//typedef unsigned char u_char; // 1바이트 메모리를 사용할 때 양수만 사용하여 값을 저장   
+//typedef unsigned short u_short; // 2바이트 메모리를 사용할 때 양수만 사용하여 값을 저장 
+//typedef unsigned int u_int;
 
 struct ethernet_header
 {
-    u_char ethernet_Dst_addr[6];
-    u_char ethernet_Src_addr[6];
+    u_char eth_Dst_mac_addr[6];
+    u_char eth_Src_mac_addr[6];
     u_short ether_type; // 2바이트  // 프로토콜 타입 설정 
-};
+} ;
 
 struct ipheader {
     unsigned char      iph_ihl:4,		// IP header length
@@ -35,29 +36,30 @@ struct ipheader {
     struct  in_addr    iph_destip;	// Destination IP address
 };
 
+/* TCP Header */
 struct tcpheader {
-    u_short t_sp;                   // source port
-    u_short t_dp;                   // destination port
-    u_short Seq;                    // sequence number
-    u_short Ack_number;             // Acknowledgemnet number
-    u_char  tcp_offx2;               // data offset, rsvd
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
     u_char  tcp_flags;
-
-    #define TH_FIN  0x01
-    #define TH_SYN  0x02
-    #define TH_RST  0x04
-    #define TH_PUSH 0x08
-    #define TH_ACK  0x10
-    #define TH_URG  0x20
-    #define TH_ECE  0x40
-    #define TH_CWR  0x80
-    #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-
-    u_short tcp_win;            //window size
-    u_short checksum;            
-    u_short urgent_pointer;     //urgent_pointer
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
 };
 
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
 
 void packet_capture(u_char* args, const struct pcap_pkthdr* header, const u_char* packet) {
 	printf("Get packet!\n");
@@ -68,12 +70,45 @@ void packet_capture(u_char* args, const struct pcap_pkthdr* header, const u_char
         // ip의 시작점을 구하는 과정 
         // = packet은 이더넷 헤더의 시작위치를 반환 + 이더넷 헤더의 크기를 하면 ip 헤더의 시작위치를 구할 수 있다. 
         struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethernet_header) + ip -> iph_ihl *4);
+        printf("Ethernet header !\n");
 
+        printf("[Destination] : ");
+        for (int i = 0; i < 6; i++) {
+            printf("%02X ", eth->eth_Dst_mac_addr[i]);
+        }
+        printf("\n");
         
-        printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   // 출발
-        printf("         To: %s\n", inet_ntoa(ip->iph_destip));    // 도착 
+        printf("[Source] : ");
+        for (int i = 0; i < 6; i++) {
+            printf("%02X ", eth->eth_Src_mac_addr[i]);
+        }
+        printf("\n");
+        
+        printf("Ip header\n");
+        printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));  
+        printf("         To: %s\n", inet_ntoa(ip->iph_destip));   
 
         printf("\n");
+
+        printf("TCP Header\n");
+        printf("Src port : %d\n", ntohs(tcp->tcp_sport));  
+        printf("Dst port : %d\n", ntohs(tcp->tcp_dport));
+
+        printf("\n");
+
+        printf("Message : \n");
+        int ip_header_len = ip -> iph_ihl *4;
+        int tcp_header_len =  TH_OFF(tcp) * 4;
+        int header_size = sizeof(struct ethernet_header) + ip_header_len + tcp_header_len;
+        int payload_size = header -> caplen - header_size;
+        const u_char *payload = packet + header_size;
+
+        printf("Payload (Max 32 bytes):\n   ");
+        for (int i = 0; i < (payload_size > 32 ? 32 : payload_size); i++) {
+            printf("%02x ", payload[i]);
+        }
+        printf("\n");
+
     }    
 }
 
